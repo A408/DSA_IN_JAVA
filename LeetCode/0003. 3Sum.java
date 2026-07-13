539dbdb4bbfc76c48c814b83f8f167c5c3ecb66d
Aman Patel <amanatifapatel29@gmail.com>
2026-07-13 18:03:10 +0530

    Solved 3Sum

---
+++ LeetCode/0003. 3Sum.java
@@ -0,0 +1,9 @@
+if (i > 0 && nums[i] == nums[i - 1])
+continue;
+for (int i = 0; i < nums.length - 2; i++) {
+Arrays.sort(nums);
+List<List<Integer>> ans = new ArrayList<>();
+public List<List<Integer>> threeSum(int[] nums) {
+class Solution {
+import java.util.*;
+int left = i + 1;
