fc1821a7b35dbd110a1936ef41827d242edefef3
Aman Patel <amanatifapatel29@gmail.com>
2026-07-22 09:00:28 +0530

    Update GFG solution: Sum of upper and lower triangles

--- GeeksForGeeks/Sum of upper and lower triangles.java
+++ GeeksForGeeks/Sum of upper and lower triangles.java
@@ -1,23 +1,28 @@
 class Solution {
     public ArrayList<Integer> sumTriangles(int mat[][]) {
-        // code here
+        int upper = 0;
+        int lower = 0;
         int n = mat.length;
-        int sum =0;
-         ArrayList<Integer> arr = new  ArrayList<Integer>();
-        for(int i=0;i<n;i++){
-            for(int j =n-1;j>=i;j--){
-                sum +=mat[i][j];
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+
+                if (i == j) {
+                    upper += mat[i][j];
+                    lower += mat[i][j];
+                }
+
+                else if (j > i)
+                    upper += mat[i][j];
+
+                else if (j < i)
+                    lower += mat[i][j];
             }
         }
-            arr.add(sum);
-            sum =0;
-           for(int i=0;i<n;i++){
-            for(int j = 0;j<=i;j++){
-                sum +=mat[i][j];
-            }
-           }
-            arr.add(sum);
-            return arr;
-        
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.add(upper);
+        result.add(lower);
+        return result;
     }
 }
