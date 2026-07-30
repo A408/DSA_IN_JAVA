97a1ced6c0845db583cfd45b4e202641cafab867
Aman Patel <amanatifapatel29@gmail.com>
2026-07-30 08:13:57 +0530

    [Syncode] GeeksForGeeks/Easy/Right Angle Triangle Pattern

---
+++ GeeksForGeeks/Easy/Right Angle Triangle Pattern/solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+      for(int i =1;i<=n;i++ ){
+        // for(int j =n;j>=i;j++){
+        //   System.out.println(" ");
+        // }
+        for(int k =1;k<=i;k++){ 
+          System.out.print("* ");
+
+        }
+        System.out.println();
+        
+      }
+        
+
+        sc.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+| # | Problem | Platform | Difficulty | Date Solved | Time |
+|---|---------|----------|------------|-------------|------|
+| 1 | [Right Angle Triangle Pattern](./GeeksForGeeks/Easy/Right%20Angle%20Triangle%20Pattern) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/right-angle-triangle-1605685807/1) | Easy | 30 Jul 2026 | 08:13 am |
