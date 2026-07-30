4fc938a4b72b22e45b268f958b26d0b073b021fc
Aman Patel <amanatifapatel29@gmail.com>
2026-07-30 09:37:27 +0530

    [Syncode] GeeksForGeeks/Easy/Max Consecutive Bit

---
+++ GeeksForGeeks/Easy/Max Consecutive Bit/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxConsecBits(int[] arr) {
+        int max = 1;
+        int count = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count > max) {
+                max = count;
+            }
+        }
+
+        return max;
+    }
+}
