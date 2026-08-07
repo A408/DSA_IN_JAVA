9cd3e19ce758be18f43797f7d01eedf648ce397d
Aman Patel <amanatifapatel29@gmail.com>
2026-08-07 20:56:16 +0530

    [Syncode] Manual/Easy/string_methods

---
+++ Manual/Easy/string_methods/README.md
@@ -0,0 +1,16 @@
+# string_methods
+
+## Difficulty: Easy
+
+## Platform: Manual
+
+## Solved On
+07 Aug 2026 at 08:56 pm
+
+11
+r
+gramming
+ogram
+
+
+## My Notes / Approach:
---
+++ Manual/Easy/string_methods/solution.py
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        String s = "Programming";
+
+System.out.println(s.length());
+System.out.println(s.charAt(4));
+System.out.println(s.substring(3));
+System.out.println(s.substring(2,7));
+        
+    }
+}
