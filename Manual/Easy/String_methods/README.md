d37821c1a22ac97556718fb8a18cbea13471482b
Aman Patel <amanatifapatel29@gmail.com>
2026-08-11 11:20:23 +0530

    [Syncode] Manual/Easy/String_methods

---
+++ Manual/Easy/String_methods/README.md
@@ -0,0 +1,31 @@
+# String_methods
+
+## Difficulty: Easy
+
+## Platform: Manual
+
+## Solved On
+11 Aug 2026 at 11:20 am
+
+8
+r
+3
+paru
+true
+false
+PARUL UU
+parul uu
+parul uu
+pardl dd
+false
+true
+false
+true
+hello1020
+90hello1020
+65
+97
+48
+hello
+
+## My Notes / Approach:
---
+++ Manual/Easy/String_methods/solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      String s = "parul uu";
+    System.out.println(s.length());
+    char ch = s.charAt(2);
+    System.out.println(ch);
+    System.out.println(s.indexOf('u'));
+    System.out.println(s.substring(0,4));
+    System.out.println(s.contains("parul"));
+    System.out.println(s.contains("abc"));
+    System.out.println(s.toUpperCase());
+    System.out.println(s.toLowerCase());
+    System.out.println(s.trim());
+    System.out.println(s.replace('u','d'));
+    System.out.println(s.startsWith("Pa"));
+    System.out.println(s.startsWith("pa"));
+    System.out.println(s.endsWith("k"));
+    System.out.println(s.endsWith("u"));
+
+     // s.equals();
+    //   s.concat();
+
+
+    System.out.println("hello"+10+20);
+    System.out.println(10+20+60+"hello"+10+20);
+
+    //ascii values
+    System.out.println((int)'A');
+    System.out.println((int)'a');
+    System.out.println((int)'0');
+
+
+    char [] w = {'h','e','l','l','o'};
+    String v1= String.valueOf(w);
+    System.out.println(v1);
+
+    }
+}
